3fa91e2ee96c7780d583a5ae9608ed451f67dea4
ChrisHwang <chrishwang@live.cn>
2017-08-06 21:50:28 +0800

    fix #284, add ProtoIncludeAttribute

---
+++ src/protobuf-net/ProtoInheritAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProtoBuf
+{
+	/// <summary>
+	/// ProtoInheritAttribute
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public class ProtoInheritAttribute : Attribute
+	{
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tag"></param>
+		public ProtoInheritAttribute(int tag)
+		{
+			Tag = tag;
+		}
+
+		/// <summary>
+		/// TagNumber
+		/// </summary>
+		public int Tag { get; set; }
+	}
+}
